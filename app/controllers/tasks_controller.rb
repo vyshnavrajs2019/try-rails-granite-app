485aca11fd4759ba0bc8884754b501ca9f94fc54
Vyshnav Raj S <svyshnavraj@gmail.com>
2021-09-05 17:10:05 +0530

    Added tasks controller and index action

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class TasksController < ApplicationController
+  def index
+    # render html: "This is index action of Tasks controller"
+
+    # @tasks = Task.all
+    # respond_to do |format|
+    #   format.html
+    #   format.xml { render xml: @tasks }
+    #   format.json { render json: @tasks }
+    # end
+  end
+end
