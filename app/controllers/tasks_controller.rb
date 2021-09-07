c2d8e89e2ca63e71ca5f3272803d0a528b4c69b2
Vyshnav Raj S <svyshnavraj@gmail.com>
2021-09-07 15:59:59 +0530

    Added API based architecture

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,8 @@ class TasksController < ApplicationController
   def index
     # render html: "This is index action of Tasks controller"
 
-    @tasks = Task.all
+    tasks = Task.all
+    render status: :ok, json: { tasks: tasks }
     # respond_to do |format|
     #   format.html
     #   format.xml { render xml: @tasks }
